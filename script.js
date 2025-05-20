710d051c2f1ee5a94b8505eb09fc894ee0791677
ColinO'Brien <colinpobrien04@gmail.com>
2025-05-19 20:32:19 -0700

    Path changes on click

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 // Handling resizing of the canvas
 var canvas = document.getElementById("myCanvas");
 var xCenter = window.innerWidth / 2;
-var yCenter = window.innerHeight / 2;
+var yCenter = parseInt((window.innerHeight / 2));
 
-// Testing some stuff out
+// Initial path
 var myPath = new Path();
 myPath.strokeColor = 'black';
 myPath.add(new Point(xCenter, yCenter));
 myPath.add(new Point(100,50));
 
-function resizeCanvas() {
-    // TODO: update positions when resizing
-    xCenter = window.innerWidth / 2;
-    yCenter = window.innerHeight / 2;
+// function resizeCanvas() {
+//     // TODO: update positions when resizing
+//     xCenter = window.innerWidth / 2;
+//     yCenter = window.innerHeight / 2;
+// }
+
+// resizeCanvas();
+// window.addEventListener("resize", resizeCanvas);
+
+var FIDELITY = 5;
+
+// Create circle of points around central point
+var movingPoint = new Point(xCenter - 100, yCenter);
+var movingPath = new Path();
+movingPath.strokeColor = 'black';
+movingPath.add(new Point(xCenter, yCenter));
+movingPath.add(movingPoint);
+
+function switchPoint() {
+    movingPath.segments[1].point.set(xCenter + 100, yCenter);
+    console.log("(" + (movingPoint.x).toString(), (movingPoint.y).toString() + ")")
 }
 
-resizeCanvas();
-window.addEventListener("resize", resizeCanvas);
+window.addEventListener('click', switchPoint);
+// Draw paths from center to each center point
