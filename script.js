ca62435952bb55ab201a4af80d23974558d24eae
ColinO'Brien <colinpobrien04@gmail.com>
2025-05-19 18:49:56 -0700

    First path

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+// Handling resizing of the canvas
+var canvas = document.getElementById("myCanvas");
+var xCenter = window.innerWidth / 2;
+var yCenter = window.innerHeight / 2;
+
+// Testing some stuff out
 var myPath = new Path();
 myPath.strokeColor = 'black';
-myPath.add(new Point(0, 0));
-myPath.add(new Point(100, 50));
+myPath.add(new Point(xCenter, yCenter));
+myPath.add(new Point(100,50));
+
+function resizeCanvas() {
+    // TODO: update positions when resizing
+    xCenter = window.innerWidth / 2;
+    yCenter = window.innerHeight / 2;
+}
+
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
