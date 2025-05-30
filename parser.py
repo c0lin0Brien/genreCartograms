1f275148aaac07e8a3916c737eff3abc12327273
ColinO'Brien <colinpobrien04@gmail.com>
2025-05-29 22:16:59 -0700

    Working example with whole globe for Blues data

--- parser.py
+++ parser.py
@@ -1,5 +1,7 @@
 import psycopg2
 import numpy as np
+import csv
+import pandas as pd
 from countries_genres_styles import genre_list, country_list
 conn = psycopg2.connect(database="discogs",
                       user="postgres",
@@ -9,17 +11,23 @@ conn = psycopg2.connect(database="discogs",
 
 master_matrix = np.zeros((len(genre_list), len(country_list)), dtype=int)
 cur = conn.cursor()
+def parser_one():
+    for curr_country in range(len(country_list)):
+        cur.execute(f"SELECT id FROM release WHERE country='{country_list[curr_country]}';")
+        ids = cur.fetchall()
+        for i in ids:
+            cur.execute(f'SELECT genre FROM release_genre WHERE release_id={i[0]};')
+            for j in cur.fetchall():
+                for k in range(len(genre_list)):
+                    if j[0] == genre_list[k]:
+                        master_matrix[k, curr_country] += 1
+    cur.close()
+    conn.close()
+    np.savetxt('test_master.csv', master_matrix, fmt='%d', delimiter=',')
 
-for curr_country in range(len(country_list)):
-    cur.execute(f"SELECT id FROM release WHERE country='{country_list[curr_country]}';")
-    ids = cur.fetchall()
-    for i in ids:
-        cur.execute(f'SELECT genre FROM release_genre WHERE release_id={i[0]};')
-        for j in cur.fetchall():
-            for k in range(len(genre_list)):
-                if j[0] == genre_list[k]:
-                    master_matrix[k, curr_country] += 1
-cur.close()
-conn.close()
-np.savetxt('test_master.csv', master_matrix, fmt='%d', delimiter=',')
+def parser_two():
+    df = pd.read_csv('../test_master_with_genres_transposed.csv')
+    df['COUNTRY'] = country_list
+    df.to_csv('../updated_master.csv', index=False)
 
+parser_two()
